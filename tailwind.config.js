ef62172acaf0e275d0450d121dd6c39d601d45a9
Nicolas Yanase <nicolasyanase18@gmail.com>
2024-04-18 21:04:32 -0300

    projeto finalizado

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./pages/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+      'color-yellow-50': '#e9a209',
+      'color-yellow-30': '#ffcb45',
+      'color-yellow-10':'#f8d477',
+      'color-brown-90': '#1d1d1d',
+      'color-brown-95': '#1d1d1dad',
+      'color-bg-': '#fff9ea',
+    },},
+  },
+  plugins: [],
+}
+
